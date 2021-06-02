f7f03c1de6b65872deb3d5abc7b8af780dc5154f
Arkady23 <a@kornienko.ru>
2021-06-02 22:08:36 +1000

    Add files via upload

--- src/cgi-bin/fmtsd.sh
+++ src/cgi-bin/fmtsd.sh
@@ -1,40 +1,45 @@
 #!/bin/sh
 
-lib=$(dirname "$0")
-. $lib/lot.sh
-
 fl=/tmp/fmt.sh
 sd=/mnt/sdcard
 dev=/dev/mmcblk0p1
+
+lib=$(dirname "$0")
+. $lib/lot.sh
+
+after_fmt(){
+  printf "#!/bin/sh\n"
+  printf "sleep 3\n"
+  printf "killall recorder\n"
+  printf "umount -l $sd\n"
+  printf "sync\n"
+  printf "L=0\n"
+  printf "while [ \$L == 0 ]; do\n"
+  printf "  sleep 2\n"
+  printf "  if [ -d \"$sd/RecFiles\" ]; then\n"
+  printf "	L=1\n"
+  printf "  fi\n"
+  printf "done\n"
+  printf "sleep 2\n"
+  printf "cd $sd\n"
+  printf "tar -x -f /tmp/cgi.tar\n"
+  printf "rm /tmp/cgi.tar\n"
+  printf "mkdir ark-add-on\n"
+  printf "cd ark-add-on\n"
+  printf "tar -x -f /tmp/ark.tar\n"
+  printf "rm /tmp/ark.tar\n"
+  printf "sync\n"
+  printf "$sd/ark-add-on/startup.sh &\n"
+}
 
 FREE_DISK=$(df -k /mnt/mtd | (read -r; read -r w1 w2 w3 w4 w5; printf "$w4"))
 USED_SPACE=$(df -h $sd | (read -r; read -r w1 w2 w3 w4; printf "$w3"))
 if [ -z $USED_SPACE ]; then
   USED_SPACE=1
 elif [ $FREE_DISK -gt 600 ]; then
-  echo '#!/bin/sh' > $fl
+  > $fl
   chmod 0777 $fl
-  echo "sleep 3" >> $fl
-  echo "killall recorder" >> $fl
-  echo "umount -l $sd" >> $fl
-  echo "sync" >> $fl
-  echo "L=0" >> $fl
-  echo "while [ \$L == 0 ]; do" >> $fl
-  echo "  sleep 2" >> $fl
-  echo "  if [ -d \"$sd/RecFiles\" ]; then" >> $fl
-  echo "	L=1" >> $fl
-  echo "  fi" >> $fl
-  echo "done" >> $fl
-  echo "sleep 2" >> $fl
-  echo "cd $sd" >> $fl
-  echo 'tar -x -f /tmp/cgi.tar' >> $fl
-  echo 'rm /tmp/cgi.tar' >> $fl
-  echo 'mkdir ark-add-on' >> $fl
-  echo 'cd ark-add-on' >> $fl
-  echo 'tar -x -f /tmp/ark.tar' >> $fl
-  echo 'rm /tmp/ark.tar' >> $fl
-  echo 'sync' >> $fl
-  echo "$sd/ark-add-on/startup.sh &" >> $fl
+  after_fmt >> $fl
   cd $sd
   tar -c -f /tmp/cgi.tar cgi-bin/ index.html.gz
   cd ark-add-on
