34893f8b5caf70e9fe3f06b721c3b351f7334919
Arkady23 <kornienko@outlook.com>
2023-07-05 13:58:46 +1000

    Update startup.sh

--- src/ark-add-on/startup.sh
+++ src/ark-add-on/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
  sd=/mnt/sdcard
  killall -9 telnetd
-# telnetd &
+ telnetd &
  $sd/bin/busybox tcpsvd -vE 0.0.0.0 21 ftpd -w &
  $sd/bin/busybox httpd -p 80 -h $sd &
- $sd/cgi-bin/offline.sh 5 &
