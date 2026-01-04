b860cf5b9dfa3dfcc1ce569306a64085fbd35402
Arkady <kornienko@outlook.com>
2026-01-05 01:26:29 +1000

    Add files via upload

--- src/ark-add-on/startup.sh
+++ src/ark-add-on/startup.sh
@@ -2,6 +2,6 @@
  sd=/mnt/sdcard
  killall -9 telnetd
  telnetd &
- watch -n 86400 $sd/cgi-bin/everyday.sh &
+ watch -n 50000 $sd/cgi-bin/everyday.sh &
  $sd/bin/busybox tcpsvd -vE 0.0.0.0 21 ftpd -w &
  $sd/bin/busybox httpd -p 80 -h $sd &
