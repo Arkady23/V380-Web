397d36aaaf37be8a080ab51dbf22320830c776dc
Arkady <kornienko@outlook.com>
2026-01-20 00:05:44 +1000

    Delete src/cgi-bin/sett.sh

--- src/cgi-bin/sett.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-printf "Content-Type: text/html\r\n\r\n"
-awk -f	/mnt/sdcard/cgi-bin/sett.awk \
-	/mnt/mtd/mvconf/factory_const.ini \
-	/mnt/sdcard/ark-add-on/startup.sh \
-	/mnt/sdcard/ark-add-on/opts.ini \
-	/mnt/mtd/mvconf/record.ini
